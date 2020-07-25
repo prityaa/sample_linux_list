e2988526c142fc355db7666dbd49c62f8faab18c
prityaa <pritam.sutar@ril.com>
2020-07-25 12:16:27 +0530

    impl geo_locate

---
+++ include/url.h
@@ -0,0 +1,26 @@
+#ifndef __URL_H__
+#define __URL_H__
+
+#define SIZE_8B		8
+#define SIZE_16B	16
+#define SIZE_256B	256
+
+const char *service_ids[] = {
+	"test-100:", 	/* MLS + Mozilla DB */
+	"test-101:", 	/* MLS + Jio DB	 */
+	"test-102:", 	/* Combain */
+	"test-103:", 	/* Skyhook */
+	"test-104:", 	/* Trilateration */
+	"test-200:", 	/* All */
+};
+
+struct struct_url {
+	char ip[SIZE_16B];
+	char operation[SIZE_16B];
+	char port[SIZE_8B];
+	char service_id[SIZE_8B];	
+	char version[SIZE_8B];
+};
+
+#endif
+
--- src/main.c
+++ src/main.c
@@ -5,25 +5,55 @@
 
 #include <rtlsRestApi.h>
 
-int main(int argc, char *argv[]) {
-    char apiKey[256] = {0};
-    int ret = 0;
-    struct getApiKeyReq_t req = {0};
-    struct getApiKeyResp_t resp = {0};
+#define DEBUG
+#include <dbg.h>
 
-    strcpy(req.uid.name, "testuser");
-    req.uid.email[0] = '\0';
-    req.uid.imei[0] = '\0';
+int get_geo_locate(const char *key);
 
-    printf("RTLS REST API implemenation\n");
-    
-    ret = rtlsGetApiKey(req, &resp);
-    
-    if(ret == 0) {
-        printf("Got get Api key, api key = %s\n", resp.apiKey);
-    } else {
-        printf("Failed to get Api key, error code %d\n", ret);
-    }
-    
-    return 0;
+void fill_req_resp_packats(struct getApiKeyReq_t *req, 
+		struct getApiKeyResp_t *resp)
+{
+	memset(req, '\0', sizeof(*req));
+	strcpy(req->uid.name, "testuser");
+	
+	memset(resp, '\0', sizeof(*resp));
+}
+
+int get_api_key(char *api_key)
+{
+	struct getApiKeyReq_t req;
+	struct getApiKeyResp_t resp;;
+
+	fill_req_resp_packats(&req, &resp);
+
+	/* request key from RTLS server */
+	if (!rtlsGetApiKey(req, &resp)) {
+		printf("\napi key = %s\n", resp.apiKey);
+		strcpy(api_key, resp.apiKey);
+	} else {
+		printf("Failed to get Api key, error code\n");
+		return -1;
+	}
+	
+	return 0;
+}
+
+int main(int argc, char *argv[]) 
+{
+	char api_key[256] = {0};
+	int ret = 0;
+
+	/* get key */	
+	if (get_api_key(api_key))
+		return -1;
+
+	//dbg("key = %s\n", api_key);
+	printf("\n\n++++++++++++++++");
+	printf(" GEO LOCATE ");
+	printf("++++++++++++++++\n\n");
+	/* get geo locate */
+	if (get_geo_locate(api_key))
+		return -2;
+
+	return 0;
 }
