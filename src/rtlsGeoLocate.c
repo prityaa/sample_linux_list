fdfab044db62edfa0e20fc65da03ecc4e6008689
prityaa <pritam.sutar@ril.com>
2020-07-29 22:16:04 +0530

    geo_locate : parsing json data that mentioned in header file.

--- src/rtlsGeoLocate.c
+++ src/rtlsGeoLocate.c
@@ -59,47 +59,109 @@ void get_url(char *url, const char *key)
 void fill_gloc_resp(json jsonRsp, struct geoLocResp_t *resp)
 {
 	/* fill statusMessage */
-
-
 #ifdef DEBUG
 	//std::cout << jsonRsp.dump(4) << std::endl;
-	std::cout << "code :"
-		<< jsonRsp["code"] << std::endl;
-	std::cout << "statusMessage :"
+	std::cout << "statusMessage (unassigned):"
 		<< jsonRsp["status"]["statusMessage"] << std::endl;
 #endif
 	std::string temp_str = jsonRsp["status"]["statusMessage"].get<std::string>();
 	strcpy(resp->statusMessage, temp_str.c_str());
+	dbg("statusMessage = %s\n", resp->statusMessage);
 
 	/* fill startTime */
 #ifdef DEBUG
 	//std::cout << jsonRsp.dump(4) << std::endl;
-	std::cout << "startTime :"
+	std::cout << "startTime (unassigned):"
 		<< jsonRsp["status"]["startTime"] << std::endl;
 #endif
 	temp_str = jsonRsp["status"]["startTime"].get<std::string>();
 	strcpy(resp->startTime, temp_str.c_str());
+	dbg("startTime = %s\n", resp->startTime);
 
 	/* fill endTime */
 #ifdef DEBUG
 	//std::cout << jsonRsp.dump(4) << std::endl;
-	std::cout << "endTime :"
+	std::cout << "endTime (unassigned):"
 		<< jsonRsp["status"]["endTime"] << std::endl;
 #endif
 	temp_str = jsonRsp["status"]["endTime"].get<std::string>();
 	strcpy(resp->endTime, temp_str.c_str());
+	dbg("endTime = %s\n", resp->endTime);
 	
+	/* fill provider */
+	resp->locations = (struct respLocation_t *)malloc
+		(sizeof(struct respLocation_t));
+
+#ifdef DEBUG
+        std::cout << "provider (unassigned):"
+                << jsonRsp["locations"][0]["provider"] << std::endl;
+#endif
+	/** TODO */
+	//temp_str = jsonRsp["locations"][0]["provider"].get<std::to_string>();
+	//strcpy(resp->locations->provider, temp_str.c_str());
+	//dbg("provider = %s\n", temp_str);
+
+	/* fill statusCode */
+#ifdef DEBUG
+        std::cout << "code (unassigned):"
+                << jsonRsp["locations"][0]["status"]["code"] << std::endl;
+#endif
+	resp->locations->statusCode = jsonRsp["locations"][0]["status"]["code"].get<int>();
+
+	dbg("resp->locations->statusCode = %d\n", 
+			resp->locations->statusCode);
 	std::cout << jsonRsp.dump(4) << std::endl;
 
-	/* fill endTime */
+	/* fill statusMsg */
 #ifdef DEBUG
-	std::cout << "code :"
-                << jsonRsp["code"] << std::endl;
-        std::cout << "accuracy :"
-                << jsonRsp["locations"] ["accuracy"] << std::endl;
+	std::cout << "statusMsg (unassigned):"
+		<< jsonRsp["locations"][0]["status"]["message"] << std::endl;
 #endif
-	//resp->locations->latitude = jsonRsp["locations"]["accuracy"].get<std::float>();
+	temp_str = jsonRsp["locations"][0]["status"]["message"].get<std::string>();
+	strcpy(resp->locations->statusMsg, temp_str.c_str());
+	dbg("statusMsg = %s\n", resp->locations->statusMsg);
 
+        /* fill latitude */
+#ifdef DEBUG
+        std::cout << "latitude (unassigned):"
+                << jsonRsp["locations"][0]["lat"] << std::endl;
+#endif
+        resp->locations->latitude = jsonRsp["locations"][0]["lat"].get<float>();
+        dbg("latitude = %f\n", resp->locations->latitude);
+
+
+        /* fill longitude */
+#ifdef DEBUG
+        std::cout << "longitude (unassigned):"
+                << jsonRsp["locations"][0]["lng"] << std::endl;
+#endif
+        resp->locations->longitude = jsonRsp["locations"][0]["lng"].get<float>();
+        dbg("longitude = %f\n", resp->locations->longitude);
+
+	/* fill accuracy */
+#ifdef DEBUG
+        std::cout << "accuracy (unassigned):"
+                << jsonRsp["locations"][0]["accuracy"] << std::endl;
+#endif
+	resp->locations->accuracy = jsonRsp["locations"][0]["accuracy"].get<float>();
+	dbg("accuracy = %f\n", resp->locations->accuracy);
+
+	/* fill code */
+#ifdef DEBUG
+        std::cout << "code (unassigned):"
+                << jsonRsp["code"] << std::endl;
+#endif
+        resp->locations->code = jsonRsp["code"].get<int>();
+        dbg("code = %d\n", resp->locations->code);
+
+	/* fill time */
+#ifdef DEBUG
+        std::cout << "statusMsg (unassigned):"
+                << jsonRsp["time"] << std::endl;
+#endif
+        temp_str = jsonRsp["time"].get<std::string>();
+        strcpy(resp->locations->time, temp_str.c_str());
+        dbg("time = %s\n", resp->locations->time);
 
 }
 
@@ -162,6 +224,7 @@ int rtls_geo_locate(struct geoLocReq_t *req,
 		jsonRsp = json::parse(rsp_data.payload);
 	
 #if 0
+		jsonRsp = json::parseFloat(rsp_data.payload);
                 std::cout << jsonRsp.dump(4) << std::endl;
                 std::cout << "Response code from json:"
                         << jsonRsp["code"] << std::endl;
